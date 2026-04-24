cec89e3f60838a6904a6fb45626bcc6074052efc
HanpenRobot <wlan>
2026-04-24 22:21:21 +0900

    feat: update csv

--- hysteresis/cube_root_dump_csv.py
+++ hysteresis/cube_root_dump_csv.py
@@ -5,6 +5,10 @@ import pandas as pd
 from sympy import Eq, solve, symbols
 
 
+def func(x: float, a: float):
+    return -3 * x + a
+
+
 L = 30
 A = list(np.arange(-L, L, 1))
 B = list(np.arange(-L, L, 1))
@@ -20,9 +24,19 @@ for a in A:
             for x in solutions
             if np.abs(complex(x).imag) < 0.001  # 虚部が十分小さい場合、実数解とみなす
         ]
-        print(f"{a=}, {b=}, {solutions=}, {len(solutions)=}")
+        count_sol = len(solutions)
+        print(f"{a=}, {b=}, {solutions=}, {count_sol=}")
         for tmp_ans in solutions:
-            results.append({"A": a, "B": b, "x": tmp_ans})
+            tmp_x = func(x=tmp_ans, a=a)
+            results.append(
+                {
+                    "A": a,
+                    "B": b,
+                    "x_e": tmp_ans,
+                    "-3(x_e)^2": tmp_x,
+                    "count_sol": count_sol,
+                }
+            )
 
 data_df = pd.DataFrame.from_dict(results, orient="columns")
 print(f"{data_df=}")
