fb015e69e75e87a6102ef96aa30b4b41918f622c
HanpenRobot <wlan>
2026-05-21 19:12:11 +0900

    feat: add vec field

---
+++ vector_field/README.md
@@ -0,0 +1,18 @@
+# 複素関数を使って2次元ベクトル場を作成
+
+
+![./vec_field_zz-1.gif](./vec_field_zz-1.gif)
+*Fig. 1 $`w=z^2-1`$のベクトル場 ($`z=x+iy`$を代入して$`w`$を求め、各$`(x,y)`$に$`w`$をベクトルとみなしてプロットした結果)*
+
+Fig. 1は[./draw_vector_field.py](./draw_vector_field.py)のコードで作成した。
+
+
+![./vec_field_zzz-1.gif](./vec_field_zzz-1.gif)
+*Fig. 2 $`w=z^3-1`$のベクトル場 ($`z=x+iy`$を代入して$`w`$を求め、各$`(x,y)`$に$`w`$をベクトルとみなしてプロットした結果)*
+
+Fig. 2は[./draw_vector_field2.py](./draw_vector_field2.py)のコードで作成した。
+
+- 参考文献[1] 方程式と対称性 山下純一 現代数学社 2021年 初版 ,pp. 228-229, pp. 234-235
+
+
+
---
+++ vector_field/draw_vector_field.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+PJ_LINE_COLOR = "#0000FF"
+X1_LINE_COLOR = "#FF0000"
+Y1_LINE_COLOR = "#00AA00"
+X_AXIS_LINE_COLOR = "#000000"
+Y_AXIS_LINE_COLOR = "#000000"
+GRID_COLOR = "#444444"
+
+LINE_WIDTH = 2
+AXIS_LINE_WIDTH = 0.8
+GRID_LINE_WIDTH = 0.8
+
+
+FIGURE_SIZE = (10, 10)
+AXIS_MAX = 10
+TITLE_FONT_SIZE = 14
+LABEL_FONT_SIZE = 16
+
+fig = plt.figure(dpi=256)
+
+plt.axes().set_aspect("equal")
+
+
+def complex_func(z: complex):
+    return z**2 - 1
+
+
+def draw(t: int):
+    print(f"plotting {t=} ...")
+    plt.cla()
+    plt.title(f"Vector field $W=Z^2-1$", fontsize=TITLE_FONT_SIZE)
+    plt.xlabel("Real part", fontsize=LABEL_FONT_SIZE)
+    plt.ylabel("Imaginary part", fontsize=LABEL_FONT_SIZE)
+    plt.grid(which="both", color=GRID_COLOR, linestyle="--", linewidth=GRID_LINE_WIDTH)
+
+    plt.axhline(0, color=X_AXIS_LINE_COLOR, linewidth=LINE_WIDTH)
+    plt.axvline(0, color=Y_AXIS_LINE_COLOR, linewidth=LINE_WIDTH)
+    plt.xlim(-AXIS_MAX, AXIS_MAX)
+    plt.ylim(-AXIS_MAX, AXIS_MAX)
+
+    L = 10
+
+    X = np.linspace(-L, L, 2 * L + 1)
+    Y = np.linspace(-L, L, 2 * L + 1)
+
+    for x in X:
+        for y in Y:
+            z = complex(real=x, imag=y)
+            res = complex_func(z)
+            print(f"{x=}, {y=}, {z=}")
+            plt.quiver(x, y, res.real, res.imag, color="#FF0000")
+
+
+N = 2
+res = FuncAnimation(fig, draw, interval=100, frames=range(N))
+
+# http://www.imagemagick.org/script/download.php#windowsのインストールが必要
+res.save("vec_field_zz-1.gif", writer="imagemagick")
