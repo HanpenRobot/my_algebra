3456f0f2f27240dcf113687e86e835a73adb0170
HanpenRobot <wlan>
2026-05-23 19:33:46 +0900

    feat: add Lotka-Volterra

---
+++ lotka_volterra_eq/README.md
@@ -0,0 +1,23 @@
+# Lotka-Volterra方程式を4次のルンゲ・クッタ法で解いた結果
+
++ Lotka-Volterra方程式を$`(1)`$,$`(2)`$で定義する。
++ 微分方程式を解く際に使用したルンゲ・クッタ法のコードは[./runge_kutta_lotka_volterra_eq.c](./runge_kutta_lotka_volterra_eq.c)である。 (このコードは参考文献[2]のコードを参考に実装した)。
+
+```math
+\frac{dx}{dt}=(a-cy)x \cdots (1)
+```
+
+```math
+\frac{dy}{dt}=-(b-dx)y \cdots (2)
+```
+
+
+![./lotka_volterra_plot.gif](./lotka_volterra_plot.gif)
+*Fig. 1 Lotka-Volterra方程式を4次のルンゲ・クッタ法で解いた結果*
+
+![./lotka_volterra_plot_anim.gif](./lotka_volterra_plot_anim.gif)
+*Fig. 2 Lotka-Volterra方程式を4次のルンゲ・クッタ法で解いた結果のアニメーション*
+
+- 参考文献[1] 常微分方程式 基礎から応用へ 新装版 俣野博 岩波書店 2026年 新装版第1刷発行, pp. 110-112
+- 参考文献[2] C言語による数値計算入門 第2版 新装版 堀之内 總一・酒井幸吉・榎園茂 森北出版株式会社 2015年 第2版装版第1刷発行, pp.128-129
+- 参考文献[3] 改定増補 カオス力学の基礎 早間 慧 現代数学社 2002年 改訂第2版, pp. 66-67
---
+++ lotka_volterra_eq/runge_kutta_lotka_volterra_eq.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double func1(double a, double c, double x, double y)
+{
+
+    return (a - c * y) * x;
+}
+
+double func2(double b, double d, double x, double y)
+{
+
+    return -(b - d * x) * y;
+}
+
+double run_runge_result(double a, double b, double c, double d, double x0, double y0)
+{
+    // 4次のルンゲ・クッタ法でlotka volterra方程式を解く
+    int i;
+    double t = 0.0;
+    double h = 0.0005; // step_size
+    double x, k1, k2, k3, k4, k;
+    double y, l1, l2, l3, l4, l;
+    int max_step = 800000;
+    double const_k = 0.2, B = 12.0;
+
+    x = x0;
+    y = y0;
+
+    FILE *fp = fopen("ans_lotka_volterra_eq.csv", "w");
+    fprintf(fp, "t,x,y,z\n");
+    for (i = 1; i < max_step; i++)
+    {
+        k1 = h * func1(a, c, x, y);
+        k2 = h * func1(a, c, x, y + l1 / 2.0);
+        k3 = h * func1(a, c, x, y + l2 / 2.0);
+        k4 = h * func1(a, c, x, y + l3);
+        k = (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
+        x = x + k;
+
+        l1 = h * func2(b, d, x, y);
+        l2 = h * func2(b, d, x + k1 / 2.0, y + l1 / 2.0);
+        l3 = h * func2(b, d, x + k2 / 2.0, y + l2 / 2.0);
+        l4 = h * func2(b, d, x + k3, y + l3);
+        l = (l1 + 2.0 * l2 + 2.0 * l3 + l4) / 6.0;
+        y = y + l;
+
+        t = t + h;
+        fprintf(fp, "%f,%f,%f\n", t, x, y);
+    }
+    return x;
+}
+
+int main(void)
+{
+
+    double a = 2.0, b = 3.0, c = 0.4, d = 1.0;
+    double x0 = 3.0, y0 = 1.0; // lotka_volterraの方程式の初期値
+    run_runge_result(a, b, c, d, x0, y0);
+
+    return 0;
+}
