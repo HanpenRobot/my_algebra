3c862acf616a09d4470f3f07d12c8585d8e883d7
HanpenRobot <wlan>
2026-06-01 21:56:18 +0900

    feat: add henon_map_per bifurcation

---
+++ henon_map/henon_map_per.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double get_distance(double x, double y, double x2, double y2)
+{
+    return sqrt(pow(x - x2, 2) + pow(y - y2, 2));
+}
+int get_period(double *tmpA1, double *tmpA2, double targetA1, double targetA2)
+{
+    double res = 0.0;
+    double epsilon = 0.000001;
+    int i = 0;
+    int max_period = 32;
+    double r_max = 1000.0;
+    for (i = 0; i < max_period; i++)
+    {
+
+        res = fabs(get_distance(tmpA1[i], tmpA2[i], targetA1, targetA2));
+        if (res < epsilon)
+        {
+            return i;
+        }
+    }
+    return -1; // 周期がmax_period以上の場合も発散したと判定する。
+}
+
+double func01(double a, double x, double y)
+{
+    return y + 1.0 - a * x * x;
+}
+
+double func02(double b, double x)
+{
+    return b * x;
+}
+
+int main(void)
+{
+    int a_max = 640, b_max = 480;
+    double x = 0.01, y = 0.01, x1, x2, y1, y2;
+    int M = 1000, N = 32, ans_per = 0;
+    double ans_R = 0.0;
+    double a_step = 2.0 / a_max, b_step = 2.0 / b_max;
+    double A1[100] = {0.0}, A2[100] = {0.0};
+
+    FILE *fp = fopen("ans_henon_map_per.csv", "w");
+    fprintf(fp, "a,b,per\n");
+
+    for (double a = 0.0; a <= 2.0; a += a_step)
+    {
+
+        for (double b = -1.0; b <= 1.0; b += b_step)
+        {
+            x1 = x;
+            y1 = y;
+            for (int i = 0; i < M; i++)
+            {
+
+                x2 = func01(a, x1, y1);
+                y2 = func02(b, x1);
+                x1 = x2;
+                y1 = y2;
+            }
+            // 周期性の判定を行うループ
+            for (int i = 0; i < N; i++)
+            {
+
+                x2 = func01(a, x1, y1);
+                y2 = func02(b, x1);
+                A1[i] = x2;
+                A2[i] = y2;
+                x1 = x2;
+                y1 = y2;
+                ans_R = fabs(get_distance(0.0, 0.0, x2, y2));
+                if (ans_R > 1000.0)
+                {
+                    ans_per = -1; // 原点からのユークリッド距離がr_max以上の場合は発散したと判定する。
+                }
+
+                ans_per = get_period(A1, A2, x2, y2);
+            }
+            printf("%f,%f,%d\n", a, b, ans_per);
+            fprintf(fp, "%f,%f,%d\n", a, b, ans_per);
+        }
+    }
+
+    return 0;
+}
---
+++ henon_map/plot_from_csv_per.py
@@ -0,0 +1,94 @@
+import csv
+from io import BytesIO
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from PIL import Image
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import pandas as pd
+
+FRAMES = 180
+fig = plt.figure(dpi=256)
+
+
+results = []
+file_name = "./ans_henon_map_per.csv"
+with open(file_name, encoding="UTF-8") as f:
+    reader = csv.reader(f)
+    num = 0
+    for line in reader:
+        if num > 0:
+
+            results.append(
+                {"a": float(line[0]), "b": float(line[1]), "per": int(line[2])}
+            )
+        num += 1
+
+df = pd.DataFrame.from_dict(results, orient="columns")
+print(f"***{df=}")
+
+
+def make_data(tmp_df, per: int):
+    tmp_df2 = tmp_df[tmp_df["per"] == per]
+    tmp_A = tmp_df2["a"].to_list()
+    tmp_B = tmp_df2["b"].to_list()
+    return tmp_A, tmp_B
+
+
+def draw(n: int):
+
+    print(f"plotting {n=} ...")
+    N_max = 2000
+    limit_const = 0.7
+    CURVE_LINE_COLOR = "#0000FF"
+    RT_LINE_COLOR = "#FF0000"
+    XY_LINE_COLOR = "#00AA00"
+    X_AXIS_LINE_COLOR = "#000000"
+    Y_AXIS_LINE_COLOR = "#000000"
+    GRID_COLOR = "#444444"
+
+    LINE_WIDTH = 1
+    AXIS_LINE_WIDTH = 0.8
+    GRID_LINE_WIDTH = 0.8
+
+    FIGURE_SIZE = (10, 10)
+    Y_AXIS_MAX = 1.0
+    Y_AXIS_MIN = -1.0
+    X_AXIS_MAX = 2.0
+    X_AXIS_MIN = 0.0
+
+    TITLE_FONT_SIZE = 14
+    LABEL_FONT_SIZE = 16
+    plt.cla()
+    plt.xlabel(r"$a$", fontsize=LABEL_FONT_SIZE)
+    plt.ylabel(r"$b$", fontsize=LABEL_FONT_SIZE, rotation=0)
+    plt.grid(which="both", color=GRID_COLOR, linestyle="--", linewidth=GRID_LINE_WIDTH)
+    # plt.xticks(np.linspace(0, 4.2, 22))
+    plt.axhline(0, color=X_AXIS_LINE_COLOR, linewidth=LINE_WIDTH)
+    plt.axvline(0, color=Y_AXIS_LINE_COLOR, linewidth=LINE_WIDTH)
+    plt.xlim(X_AXIS_MIN, X_AXIS_MAX)
+    plt.ylim(Y_AXIS_MIN, Y_AXIS_MAX)
+
+    res_A, res_B = make_data(tmp_df=df, per=0)
+    plt.plot(res_A, res_B, ".", ms=1, color="#0000FF")
+
+    res_A2, res_B2 = make_data(tmp_df=df, per=1)
+    plt.plot(res_A2, res_B2, ".", ms=1, color="#00AA00")
+
+    res_A3, res_B3 = make_data(tmp_df=df, per=2)
+    plt.plot(res_A3, res_B3, ".", ms=1, color="#00AAAA")
+
+    res_A4, res_B4 = make_data(tmp_df=df, per=3)
+    plt.plot(res_A4, res_B4, ".", ms=1, color="#FF0000")
+
+    res_A5, res_B5 = make_data(tmp_df=df, per=-1)
+    plt.plot(res_A5, res_B5, ".", ms=1, color="#000000")
+
+    # plt.legend(loc="lower center", borderaxespad=1, fontsize=10)
+
+
+FRAMES = 1
+res = FuncAnimation(fig, draw, interval=50, frames=range(FRAMES))
+
+# http://www.imagemagick.org/script/download.php#windowsのインストールが必要
+res.save(f"ans_henon_map_per.gif", writer="imagemagick")
