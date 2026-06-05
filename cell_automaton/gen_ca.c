d1f27a78d0fc926baa45866c2e39ac214df7d1c4
HanpenRobot <wlan>
2026-06-05 20:30:46 +0900

    refactor: gen_ca.c

--- cell_automaton/gen_ca.c
+++ cell_automaton/gen_ca.c
@@ -32,7 +32,6 @@ int get_max(int *array)
 
     for (int i = 0; i < 6; i++)
     {
-        // printf("array[%d]=%d\n", i, array[i]);
         if (ans < array[i])
         {
             ans = array[i];
@@ -40,49 +39,6 @@ int get_max(int *array)
     }
     return ans;
 }
-
-// int main()
-// {
-//     int tmp_array[5] = {0};
-//     int res = 0;
-//     tmp_array[0] = -3;
-//     tmp_array[1] = 2;
-//     tmp_array[2] = 1;
-//     res = get_max(tmp_array);
-
-//     printf("res=%d\n", res);
-//     return 0;
-// }
-
-// int main()
-// {
-//     int tmp_array[10] = {0};
-//     int tmp_array_before[10] = {0};
-//     for (int i = 0; i < 10; i++)
-//     {
-//         tmp_array[i] = i + 1;
-//     }
-//     for (int i = 0; i < 10; i++)
-//     {
-//         if (i > 1)
-//         {
-//             tmp_array_before[i] = tmp_array[i - 1];
-//             printf("tmp_array[%d]=%d , tmp_array_before[%d]=%d \n ", i, tmp_array[i], i, tmp_array_before[i]);
-//         }
-//         else
-//         {
-//             printf("tmp_array[%d]=%d \n ", i, tmp_array[i]);
-//         }
-//     }
-//     // int res = 0;
-//     // tmp_array[0] = -3;
-//     // tmp_array[1] = 2;
-//     // tmp_array[2] = 1;
-//     // res = get_max(tmp_array);
-
-//     // printf("res=%d\n", res);
-//     return 0;
-// }
 
 int main()
 {
@@ -150,8 +106,6 @@ int main()
             for (int jj = 0; jj < 100; jj++)
             {
                 tmpA[ii][jj] = 0;
-                // tmpA2[ii][jj] = 0;
-                // tmpA3[ii][jj] = 0;
             }
         }
         for (int ii = 0; ii < N; ii++)
@@ -160,19 +114,9 @@ int main()
             {
                 tmpA[ii][jj] = U[ii][jj]; // f_cntの時の情報
                 U_history[f_cnt][ii][jj] = tmpA[ii][jj];
-                // tmpA2[ii][jj] = U[ii][jj];
-                // tmpA3[ii][jj] = tmpA[ii][jj];
             }
         }
-        // for (int ii = 0; ii < N; ii++)
-        // {
-        //     for (int jj = 0; jj < N; jj++)
-        //     {
-        //         U_history[f_cnt][ii][jj] = tmpA[ii][jj]; // f_cntの時の情報　(どうやったらf_cnt-1の情報を保存できるか?)
-        //         // tmpA2[ii][jj] = U[ii][jj];
-        //         // tmpA3[ii][jj] = tmpA[ii][jj];
-        //     }
-        // }
+
         for (int i = 1; i < N - 1; i++)
         {
             for (int j = 1; j < N - 1; j++)
@@ -185,79 +129,10 @@ int main()
                 array[4] = tmpA[i][j + 1];
                 array[5] = U_history[f_cnt - 1][i][j];
                 res_local = get_max(array) - U_history[f_cnt - 1][i][j];
-                // if (f_cnt > 1)
-                // {
-                //     array[5] = U_history[f_cnt - 2][i][j];
-                //     res_local = get_max(array) - U_history[f_cnt - 2][i][j];
-                // }
-                // else
-                // {
-                //     array[5] = tmpA[i][j + 1];
-                //     res_local = get_max(array);
-                // }
 
                 U[i][j] = res_local; // f_cntがf_cnt+1の時の情報に更新される
             }
         }
-
-        //     for (int i = 1; i < N - 1; i++)
-        //     {
-
-        //         int array[6] = {0};
-        //         for (int ii = 0; ii < N; ii++)
-        //         {
-        //             for (int jj = 0; jj < N; jj++)
-        //             {
-        //                 tmpA[ii][jj] = U[i][jj];
-        //             }
-        //         }
-
-        //         for (int j = 1; j < N - 1; j++)
-        //         {
-
-        //             // if (U[i][j] == 1)
-        //             // {
-        //             //     int res_local = 0;
-        //             //     array[0] = tmpA[i][j];
-        //             //     array[1] = tmpA[i + 1][j];
-        //             //     array[2] = tmpA[i - 1][j];
-        //             //     array[3] = tmpA[i][j - 1];
-        //             //     array[4] = tmpA[i][j + 1];
-        //             //     array[5] = tmpA_before[i][j];
-        //             //     res_local = get_max(array) - tmpA_before[i][j];
-        //             //     tmpA[i][j] = res_local - tmpA_before[i][j];
-        //             //     printf("f_cnt=%d, frame_num=%d,i=%d,j=%d,A=%d,res_max=%d\n", f_cnt, frame_num, i, j, U[i][j], res_local);
-        //             //     // tmpA[i][j] = 0;
-        //             //     // tmpA[i][j + 1] = 1;
-        //             // }
-        //             int res_local = 0;
-        //             array[0] = tmpA[i][j - 1];
-        //             array[1] = tmpA[i][j - 1];
-        //             array[2] = tmpA[i][j + 1];
-        //             array[3] = tmpA[i][j + 1];
-        //             array[4] = tmpA[i][j];
-        //             array[5] = tmpA2[i][j];
-        //             res_local = get_max(array);
-        //             tmpA[i][j] = res_local - tmpA2[i][j];
-        //             // printf("f_cnt=%d, frame_num=%d,i=%d,j=%d,A=%d,res_max=%d\n", f_cnt, frame_num, i, j, U[i][j], res_local);
-        //         }
-
-        //         for (int j = 0; j < N - 1; j++)
-        //         {
-        //             U[i][j] = tmpA[i][j];
-        //         }
-        //         if (f_cnt > 0)
-        //         {
-        //             for (int ii = 0; ii < N; ii++)
-        //             {
-        //                 for (int jj = 0; jj < N; jj++)
-        //                 {
-        //                     // tmpA[ii][jj] = U[ii][jj];
-        //                     tmpA2[ii][jj] = U[ii][jj];
-        //                 }
-        //             }
-        //         }
-        //     }
     }
 
     return 0;
